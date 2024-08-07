bc2df4bdfb234036841b841c12b7f30be3af2d0f
Adaeze Ugwumba <adaezeugwumba460@gmail.com>
2024-08-07 15:57:57 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Count from  "./pages/Count"
+import "./App.css";
+import StudentForm from "./pages/StudentForm";
+
+
+function App() {
+ 
+  return (
+    <div>
+      <Count />
+      <StudentForm />
+    </div>
+  );
+}
+
+export default App;
